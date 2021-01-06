839f761598e4a4b7c8ed6f38396b5a46821c0445
elisa-desmetz <71330692+elisa-desmetz@users.noreply.github.com>
2021-01-06 12:35:46 +0100

    Add files via upload

--- ml/utils/utils.py
+++ ml/utils/utils.py
@@ -16,6 +16,7 @@ class DataHandling:
     def get_data(self):
         print("Loading data from local file...")
         self.data = pd.read_csv("./static/data/data.csv")
+        self.data=self.data.rename(columns={'Engine HP':'HP','Engine Cylinders':'Cylinders','Transmission Type':'Transmission','Driven_Wheels':'Drive Mode','highway MPG':'MPG-H','city mpg':'MPG-C','MSRP':'Price'})
         print("Dataset shape : {} lines, {} columns".format(self.data.shape[0],self.data.shape[1]))
         print("Data loaded from local file !")
 
@@ -35,19 +36,36 @@ class FeatureRecipe:
         self.drop = []
         print("Initialization done !")
 
+    def convert_and_encode(self):
+        def getrange(Price):
+            if (Price >= 0 and Price < 25000):
+                return '0 - 25000'
+            if (Price >= 25000 and Price < 50000):
+                return '25000 - 50000'
+            if (Price >= 50000 and Price < 75000):
+                return '50000 - 75000'
+            if (Price >= 75000 and Price < 100000):
+                return '75000 - 100000'
+        self.data['Price Range'] = self.data.apply(lambda x:getrange(x['Price']),axis = 1)
+
+        label_encoder = preprocessing.LabelEncoder()
+        for col in ['Make','Model','Engine Fuel Type','Transmission','Drive Mode','Vehicle Size','Vehicle Style','Price']:
+            self.data[col] = self.data[col].astype('category') 
+            self.data[col] = label_encoder.fit_transform(self.data[col])
+
     def separate_variable_types(self) -> None:
         """
             Separating dataset features by type
         """
         print("Separating features by type...")
         for col in self.data.columns:
-            if self.data[col].dtypes == int:
+            if self.data[col].dtypes == 'int64':
                 self.discrete.append(self.data[col])
-            elif self.data[col].dtypes == float:
+            elif self.data[col].dtypes == 'float64':
                 self.continuous.append(self.data[col])
             else:
                 self.categorial.append(self.data[col])
-        print ("Dataset columns : {} \nNumber of discrete features : {} \nNumber of continuous features : {} \nNumber of categorical features : {} \nTotal number of features : {}".format(len(self.data.columns),len(self.discrete),len(self.continuous),len(self.categorial),len(self.discrete)+len(self.continuous)+len(self.categorial) ))
+        print ("Dataset columns : {} \nNumber of discrete features : {} \nNumber of continuous features : {} \nNumber of categorical features : {} \nTotal number of features : {}\n".format(len(self.data.columns),len(self.discrete),len(self.continuous),len(self.categorial),len(self.discrete)+len(self.continuous)+len(self.categorial) ))
 
     def drop_useless_features(self) :
         """
@@ -55,10 +73,19 @@ class FeatureRecipe:
         """
         print("Dropping useless features and observations...")
         # Dropping observations with price == 0
-        self.data.drop(self.data[self.data['MSRP'] == 0].index, inplace=True)
+        self.data.drop(self.data[self.data['Price'] == 0].index, inplace=True)
         # Dropping Market Cetegory feature
         self.data.drop(['Market Category'], axis=1, inplace=True)
-        print("Useless features and observations dropped !")
+        # Dropping duplicates observations and null features observations
+        self.data.drop_duplicates(keep=False,inplace=True)
+        self.data.dropna(inplace=True,axis=0)
+        # Dropping extreme observations
+        self.data.drop(self.data[self.data['Price'] >= 500000].index,inplace=True)
+        Q1 = self.data.quantile(0.25)
+        Q3 = self.data.quantile(0.75)
+        IQR = Q3 - Q1
+        self.data = self.data[~((self.data < (Q1 - 1.5*IQR))|(self.data > (Q3 + 1.5*IQR))).any(axis = 1)]
+        print("Useless features and observations dropped !\n")
 
     def drop_duplicate(self) :
         """
@@ -108,14 +135,15 @@ class FeatureRecipe:
             if self.data[col].isna().sum()/self.data.shape[0] >= threshold:
                 self.drop.append( self.data.drop([col], axis='columns', inplace=True) )
                 count+=1
-        print("Dropped {} features.".format(count))
+        print("Dropped {} features.\n".format(count))
 
     def prepare_data(self,threshold : float):
         self.drop_useless_features()
         self.drop_na_prct(threshold)
         self.drop_duplicate()
         self.separate_variable_types()
-        print(self.data.shape)
+        self.convert_and_encode()
+        print("Processed dataset shape : {} lines, {} columns".format(self.data.shape[0],self.data.shape[1]))
         print("FeatureRecipe processing done !\n")
 
 
